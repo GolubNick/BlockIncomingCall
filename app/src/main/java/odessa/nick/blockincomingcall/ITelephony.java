33646327d563a83440b47eab2fe55eb0e284bde9
Nick <golub.nick@gmail.com>
2019-10-06 18:37:33 +0300

    Initial commit

---
+++ app/src/main/java/odessa/nick/blockincomingcall/ITelephony.java
@@ -0,0 +1,8 @@
+package odessa.nick.blockincomingcall;
+
+public interface ITelephony {
+
+    boolean endCall();
+    void answerRingingCall();
+    void silenceRinger();
+}
---
+++ app/src/main/java/odessa/nick/blockincomingcall/MainActivity.java
@@ -0,0 +1,17 @@
+package odessa.nick.blockincomingcall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
